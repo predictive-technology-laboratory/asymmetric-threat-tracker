83ed4ea08febf5882b054f82b3ef9df8c2fe4cef
MatthewGerber <gerber.matthew@gmail.com>
2014-08-15 11:03:15 -0400

    Hangs

--- nunit.sh
+++ nunit.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $1
+
+runTest(){
+   mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+   if [ $? -ne 0 ]
+   then   
+     exit 1
+   fi
+}
+
+runTest AttTest/bin/Release/AttTest.dll
+runTest GuiTest/bin/Release/GuiTest.dll
+
 exit $?
