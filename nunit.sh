cf438f4ccf60b2465297d6dafb0a3491e5f0ca48
MatthewGerber <gerber.matthew@gmail.com>
2014-08-15 11:40:39 -0400

    Hangs no more?

--- nunit.sh
+++ nunit.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 runTest(){
-   mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+   
    if [ $? -ne 0 ]
    then   
      exit 1
    fi
 }
 
-runTest AttTest/bin/Release/AttTest.dll
+runTest 
 echo "Running GuiTest.dll"
-runTest GuiTest/bin/Release/GuiTest.dll
+runTest 
 
 exit $?
