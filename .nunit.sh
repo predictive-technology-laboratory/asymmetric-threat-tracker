eb2183ddefb2f6292abff31020be15d06b31b372
Matthew Gerber <gerber.matthew@gmail.com>
2014-08-18 00:03:18 -0400

    Hangs

--- .nunit.sh
+++ .nunit.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe $1
+output=$(mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe $1)
+echo $output
 echo "Done"
 exit $?
