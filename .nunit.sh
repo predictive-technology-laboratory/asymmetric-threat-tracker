9e37a4237b69a0f854273e444bb3249ca4cdef12
Matthew Gerber <gerber.matthew@gmail.com>
2014-08-17 23:50:00 -0400

    Hangs

--- .nunit.sh
+++ .nunit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe $1 -noxml -labels -stoponerror -trace=Verbose
+mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe $1 -noxml -labels -stoponerror -trace=Off -nologo -apartment=STA
 echo "Done"
 exit $?
