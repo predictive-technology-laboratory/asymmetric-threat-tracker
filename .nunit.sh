894cb592d0b6d7f0e47ec2ca05d956ba2b032d18
Matthew Gerber <gerber.matthew@gmail.com>
2014-08-17 23:41:08 -0400

    Hangs

--- .nunit.sh
+++ .nunit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe $1 -noxml -labels -stoponerror -trace=Verbose -out=out.txt -err=err.txt
+mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe $1 -noxml -labels -stoponerror -trace=Verbose
 echo "Done"
 exit $?
