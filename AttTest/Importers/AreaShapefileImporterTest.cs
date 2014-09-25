d9f3018fbdeb81e35ce4df6f4ca262b8eb6c18f6
Matthew Gerber <msg8u@virginia.edu>
2014-09-25 16:16:21 -0400

    Initial steps on unit testing.

--- AttTest/Importers/AreaShapefileImporterTest.cs
+++ AttTest/Importers/AreaShapefileImporterTest.cs
@@ -1,17 +1,29 @@
 ﻿using NUnit.Framework;
 using PTL.ATT;
+using PTL.ATT.Importers;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Compression;
 
 namespace AttTest.Importers
 {
     [TestFixture]
     public class AreaShapefileImporterTest
     {
+        private static string AreaZipFilePath
+        {
+            get { return Path.Combine(Configuration.PostGisShapefileDirectory, "Chicago", "AO", "chicago_city_boundary.zip"); }
+        }
+
+        private static string UnzippedAreaDirectory
+        {
+            get { return Path.Combine(AreaZipFilePath, "_unzipped"); }
+        }
+
         [TestFixtureSetUp]
         public void SetUp()
         {
@@ -19,16 +31,31 @@ namespace AttTest.Importers
                 Configuration.Reset(null);
             else
                 Configuration.Initialize("att_config.xml", true);
+
+            UnzipTestArea();
         }
 
         [Test]
         public void Test()
         {
+            ImportTestArea();
+            Area importedArea1 = Area.GetAll().Last();
+            ImportTestArea();
+            Area importedArea2 = Area.GetAll().Last();
+            Assert.AreEqual(importedArea1.Id, 1);
+            Assert.AreEqual(importedArea2.Id, 2);
+            Assert.AreEqual(Area.GetAll().Count, 2);
         }
 
-        [TestFixtureTearDown]
-        public void TearDown()
+        public static void UnzipTestArea()
         {
+            if (!Directory.Exists(UnzippedAreaDirectory))
+                ZipFile.ExtractToDirectory(AreaZipFilePath, UnzippedAreaDirectory);
+        }
+
+        public static void ImportTestArea()
+        {
+            new AreaShapefileImporter("Chicago", Path.Combine(UnzippedAreaDirectory, "chicago_city_boundary.shp"), null, null, -1, -1, null, 1000).Import();
         }
     }
 }
--- AttTest/Importers/FeatureShapefileImporterTest.cs
+++ AttTest/Importers/FeatureShapefileImporterTest.cs
@@ -1,8 +1,10 @@
 ﻿using NUnit.Framework;
 using PTL.ATT;
+using PTL.ATT.Importers;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,11 @@ namespace AttTest.Importers
     [TestFixture]
     public class FeatureShapefileImporterTest
     {
+        public static string FeaturesDirectory
+        {
+            get { return Path.Combine(Configuration.PostGisShapefileDirectory, "Chicago", "Features"); }
+        }
+
         [TestFixtureSetUp]
         public void SetUp()
         {
@@ -19,16 +26,36 @@ namespace AttTest.Importers
                 Configuration.Reset(null);
             else
                 Configuration.Initialize("att_config.xml", true);
+
+            UnzipTestFeatures();
         }
 
         [Test]
         public void Test()
         {
+            ImportTestFeatureShapefiles();
         }
 
-        [TestFixtureTearDown]
-        public void TearDown()
+        public static void UnzipTestFeatures()
         {
+            foreach (string featureZipFilePath in Directory.GetFiles(FeaturesDirectory, "*.zip"))
+            {
+                string unzippedPath = featureZipFilePath + "_unzipped";
+                if (!Directory.Exists(unzippedPath))
+                    ZipFile.ExtractToDirectory(featureZipFilePath, unzippedPath);
+            }
+        }
+
+        public static void ImportTestFeatureShapefiles()
+        {
+            int shapefileCount = 0;
+            foreach (string distanceShapefilePath in Directory.GetFiles(FeaturesDirectory, "*.shp"))
+            {
+                new FeatureShapefileImporter(Path.GetFileName(distanceShapefilePath), distanceShapefilePath, null, null, -1, -1, null).Import();
+                ++shapefileCount;
+            }
+
+            Assert.AreEqual(shapefileCount, Shapefile.GetAll().Count());
         }
     }
 }
